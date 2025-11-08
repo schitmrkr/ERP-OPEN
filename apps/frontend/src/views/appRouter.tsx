be38365d90273ef55bae46617176749d9a3d8888
schitmrkr <schitmrkr@gmail.com>
2025-11-09 00:12:38 +0545

    feat: organization with owner bootstrap

--- apps/frontend/src/views/appRouter.tsx
+++ apps/frontend/src/views/appRouter.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginView from "./routes/auth/loginView";
-import RegisterView from "./routes/auth/registerView";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginView />} />
-        <Route path="/register" element={<RegisterView />} />
       </Routes>
     </Router>
   );
